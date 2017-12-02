b3a648b7a2dea2ebcd678e879e88ce85af8cfafa
5chdn <5chdn@users.noreply.github.com>
2017-12-02 23:31:07 +0100

    Refactor trip rating to master contract containing all trips

--- contracts/tripRater.sol
+++ contracts/tripRater.sol
@@ -3,87 +3,93 @@ pragma solidity ^0.4.19;
 // initilizes a new trip and rates quality
 contract tripRater {
 
-    // account of the truck iot device tracking the trip (owner)
-    address truckDevice;
+    struct Trip {
+        // account of the truck iot device tracking the trip (owner)
+        address truckDevice;
 
-    // quality of the trip. 100000 is perfect. 0 is bad, probably failed.
-    uint256 tripQuality;
+        // quality of the trip. 100000 is perfect. 0 is bad, probably failed.
+        int256 tripQuality;
 
-    // tracks the state of the light sensor
-    uint256 lightIllumilation;
+        // tracks the state of the light sensor
+        uint256 lightIllumilation;
 
-    // tracks the sate of the z-axis accelerometer
-    uint256 zAcceleration;
+        // tracks the sate of the z-axis accelerometer
+        uint256 zAcceleration;
 
-    // keeps track of the latest logged event
-    uint256 latestEvent;
+        // keeps track of the latest logged event
+        uint256 latestEvent;
 
-    // trip status, is false if not started or already stopped
-    bool isDriving;
+        // trip status, is false not at destination yet
+        bool isFinalized;
+    }
 
-    // allows only the iot device of the truck to modify state
-    modifier only_truck { require (msg.sender == truckDevice); _; }
+    mapping (uint => Trip) allTrips;
+
+    uint256 numTrips;
 
     // constructor, sets trip owner and quality
-    function tripRater (uint256 _light, uint256 _z) public {
-        truckDevice = msg.sender;
-        lightIllumilation = _light;
-        zAcceleration = _z;
-        startTrip();
+    function tripRater () public {
+        numTrips = 0;
     }
 
     // the truck is starting the trip
-    function startTrip() internal {
-
-        // initialize at 100.000%
-        tripQuality = 100000;
-
-        // start the truck
-        isDriving = true;
+    function newTrip (uint256 _light, uint256 _z) public returns (uint256) {
+        uint256 tripID = numTrips + 1;
+        allTrips[tripID] = Trip(msg.sender, 100000, _light, _z, 0, false);
+        return tripID;
     }
 
     // finalizes the trip once the truck arrives
-    function finalizeTrip () public only_truck {
-
-        // stop the truck
-        isDriving = false;
+    function finalizeTrip (uint256 _id) public {
+        if (msg.sender == allTrips[_id].truckDevice) {
+            // stop the truck
+            allTrips[_id].isFinalized = true;
+        }
     }
 
     // tracks changes in light events inside the trailer
-    // {"sensorType": "LIGHT", "valueLength": 1, "values": 0.96, "timestamp": 1512052879014, "": ""}
-    function trackLightEvent (uint256 _time, uint256 _light) public only_truck {
-        if (msg.sender == truckDevice && isDriving) {
+    function trackLightEvent (uint256 _id, uint256 _time, uint256 _light) public {
+        if (msg.sender == allTrips[_id].truckDevice && !allTrips[_id].isFinalized) {
 
             // rating reduces by -10.000%
-            tripQuality = tripQuality - 10000;
+            int256 quality = allTrips[_id].tripQuality -10000;
+            if (quality > 0) {
+                allTrips[_id].tripQuality = quality;
+            } else {
+                allTrips[_id].tripQuality = 0;
+            }
 
             // set light and time
-            lightIllumilation = _light;
-            latestEvent = _time;
+            allTrips[_id].lightIllumilation = _light;
+            allTrips[_id].latestEvent = _time;
         }
     }
 
     // tracks bumpy road events based z-acceleromator
-    // {"sensorType": "ACCELEROMETER", "valueLength": 3, "values": [0.09375, 0.055419921875, -0.912109375], "timestamp": 1512052880297, "": ""}
-    function trackBumpEvent (uint256 _time, uint256 _z) public only_truck {
-        if (msg.sender == truckDevice && isDriving) {
+    function trackBumpEvent (uint _id, uint256 _time, uint256 _z) public {
+        if (msg.sender == allTrips[_id].truckDevice && !allTrips[_id].isFinalized) {
 
-            // rating reduces by -0.001%
-            tripQuality = tripQuality - 1;
+            // rating reduces by -0.1%
+            int256 quality = allTrips[_id].tripQuality -100;
+            if (quality > 0) {
+                allTrips[_id].tripQuality = quality;
+            } else {
+                allTrips[_id].tripQuality = 0;
+            }
 
             // set bump intensity and time
-            zAcceleration = _z;
-            latestEvent = _time;
+            allTrips[_id].zAcceleration = _z;
+            allTrips[_id].latestEvent = _time;
         }
     }
 
     // allow calling the truck status
-    function isTruckDriving () constant public returns (bool) {
-        return isDriving;
+    function isTripFinalized (uint256 _id) constant public returns (bool) {
+        return allTrips[_id].isFinalized;
     }
 
     // allow calling the trip quality
-    function getTripRating () constant public returns (uint256) {
-        return tripQuality;
+    function getTripRating (uint256 _id) constant public returns (int256) {
+        return allTrips[_id].tripQuality;
     }
 }
